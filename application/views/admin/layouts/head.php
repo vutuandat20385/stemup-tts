19b637af12944ac3cf3d78469a3bce6079d49677
dung21121995 <dugk3nlee@gmail.com>
2020-03-24 11:21:20 +0700

    update
    Signed-off-by: dung21121995 <dugk3nlee@gmail.com>

--- application/views/admin/layouts/head.php
+++ application/views/admin/layouts/head.php
@@ -12,7 +12,7 @@
     <link type="text/css" href="<?php echo base_url('edmin/css/theme.cs'); ?>s" rel="stylesheet">
     <link type="text/css" href="<?php echo base_url('edmin/images/icons/css/font-awesome.css'); ?>" rel="stylesheet">
     <link type="text/css" href="<?php echo base_url('css/admin/style-admin.css'); ?>" rel="stylesheet">
-
+     <script type="text/javascript" src="<?php echo base_url();?>editor/tinymce.min.js"></script>
     <script src="<?php echo base_url('ckeditor/ckeditor.js'); ?>" type="text/javascript"></script>
     <script>
         base_url="<?php echo base_url(); ?>";
