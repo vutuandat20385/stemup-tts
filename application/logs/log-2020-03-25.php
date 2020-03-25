2d2d0cfc92b3389edf6ad055add2f404a9bb8265
dung21121995 <dugk3nlee@gmail.com>
2020-03-25 09:14:15 +0700

    dung
    Signed-off-by: dung21121995 <dugk3nlee@gmail.com>

---
+++ application/logs/log-2020-03-25.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2020-03-25 08:45:09 --> Cache: Failed to initialize APC; extension not loaded/enabled?
+ERROR - 2020-03-25 08:51:48 --> Severity: Warning --> session_id(): Cannot change session id when session is active C:\xampp\htdocs\stemup-tts\application\libraries\Session.php 53
+ERROR - 2020-03-25 08:53:55 --> Cache: Failed to initialize APC; extension not loaded/enabled?
+ERROR - 2020-03-25 08:53:56 --> Cache: Failed to initialize APC; extension not loaded/enabled?
+ERROR - 2020-03-25 08:54:02 --> Cache: Failed to initialize APC; extension not loaded/enabled?
+ERROR - 2020-03-25 08:54:02 --> Cache: Failed to initialize APC; extension not loaded/enabled?
